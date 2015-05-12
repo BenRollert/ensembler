125030c86036d4784daf1421a713e0adcaa4bcc1
Benjamin Rollert <brollert@studioqi.ca>
2015-05-12 15:40:53 -0400

    documenting key functions

---
+++ R/ensembler.R
@@ -0,0 +1,14 @@
+#' ensembler: An R package for building ensemble methods with Domino Datalab (www.dominoup.com).
+#'
+#' Domino Datalab (www.dominoup.com) offers R users a 
+#' way to programmatically trigger runs on cloud-hosted infrastructure with its package, domino. 
+#' ensemlber extends domino's functionality by integrating with the well-known caret package to enable
+#' users to train models on seperate AWS instances in the cloud. ensembler also offers a clean interface for downloading and combining 
+#' Rdata files from Domino. 
+#' 
+#' @section ensembler functions:
+#' The ensembler functions launcher, loadResults, compare, modelList, predict.ensemble
+#'
+#' @docType package
+#' @name ensembler
+NULL
--- R/model_func.R
+++ R/model_func.R
@@ -1,20 +1,45 @@
-#' Load results from a caret train object saved in an .Rda file; can assign output to a variable.
+#' Load results from a caret train object saved in an .Rda file.
+#' 
+#' Loads an Rda file in the enclosing function environment.
+#' Allows for assignment of the Rda file to a variable, without polluting the global environment.
 #' 
 #' @param dataset A character string specifying the dataset associated with the desired results object.
 #' @param par A character string specifying the parameter or model associated with the desired results object.
 #' @return The 'results' slot from a caret train object.
 #' @examples
 #' results <- loadResults("BreastCancer", "nnet")
+#' @export
 loadResults <- function(dataset, par){
 load(sprintf("results_%s_%s.Rda", dataset, par))
 results
 }
 
-
+#' Programmatically pass arguments to a script for training caret models.
+#' 
+#' @param script A character string specifying the name of the script you wish to run.
+#' @param dataset A character string specifying the name of the dataset you are training the models on.
+#' @param par Character string vector specifying the parameters of interest. Examples of parameters include different values of hyperparameters or different caret methods.
+#' @return Response from Domino indicating if runs are succesfully triggered. 
+#' @examples
+#' models <- c("nnet", "rf", "gbm")
+#' launcher(script = "variable_model.R", dataset = "BreastCancer", par = models)
+#' @export
 launcher <- function(script, dataset, par){
   lapply(par, function(m) domino.run(script, dataset, m))
 }
 
+#' Compare results from caret models trained on multiple instances on Domino.
+#' 
+#' Assumes results are downloaded as .Rda files to a single machine. Loads Rda and files and rbinds the results objects into a single data.frame for the sake of comparison.
+#' 
+#' @param dataset A character string specifying the name of the dataset you are training the models on.
+#' @param par Character string vector specifying the parameters of interest. Examples of parameters include different values of hyperparameters or different caret methods.
+#' @param metric A character string specifying the metric you wish to rank caret results by. 
+#' @return data.frame containing results from caret train objects, ranked in descending order according to the chosen accuracy metric.
+#' @examples
+#' models <- c("nnet", "rf", "gbm")
+#' compare(dataset = "BreastCancer", par = models, metric = "ROC")
+#' @export
 compare <- function(dataset, par, metric){
   result <- lapply(par, loadResults, dataset=dataset)
   result <- do.call(rbind, result)
@@ -27,14 +52,35 @@ load_models <- function(dataset, method){
   model
 }
 
-
+#' Create a list of caret train objects trained on multiple Domino instances.
+#' 
+#' Assumes caret train objects are downloaded as .Rda files to a single machine. Loads Rda files and returns all caret models in a single list.
+#' 
+#' @param dataset A character string specifying the name of the dataset you are training the models on.
+#' @param models Character string vector specifying the names of the models you wish to load.
+#' @return a list of class "ensemble" containing caret train objects.
+#' @examples
+#' models <- c("nnet", "rf", "gbm")
+#' modelList(dataset = "BreastCancer", models = models)
+#' @export
 modelList <- function(dataset, models){
   ens <- lapply(models, load_models, dataset=dataset)
   class(ens) <- "ensemble"
   ens
 }
 
-
+#' Predict method for objects of class "ensemble"
+#' 
+#' 
+#' @param model_list An object of class "ensemble" containing a list of caret train objects
+#' @param newdata A data.frame containing the features you wish to predict on. 
+#' @param type A character string specifying the response; for now only supports "prob"
+#' @param outcome An integer indicating the column index to select from each model's prediction matrix. 
+#' @return a matrix of predictions, with each column vector corresponding to a different model.
+#' @examples
+#' model_list <- modelList(dataset="BreastCancer", models=models)
+#' pred_mat <- predict(model_list, newdata=testing, type="prob", outcome=2)
+#' @export
 predict.ensemble <- function(model_list, newdata, type, outcome){
   
   pred_fact <- function(newdata, type, outcome){
